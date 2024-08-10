2d982b47bdbfea2a5a096a368b0e3b9cab256a82
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-08-10 22:41:12 +0300

    add allure

--- tests/test_courier.py
+++ tests/test_courier.py
@@ -1,10 +1,14 @@
+import allure
 import pytest
 import requests
 
 from helpers import url, data
 
 
+@allure.feature("Courier")
 class TestCreateCourier:
+    @allure.story("Успешное создание учетной записи курьера")
+    @allure.title("Создание учетной записи курьера со всеми полями или без поля firstName")
     @pytest.mark.parametrize("data_courier, expected_status_code, expected_response", [
         (data.generate_data_courier(), 201, {'ok': True}),  # Все поля переданы
         (data.generate_data_courier(include_first_name=False), 201, {'ok': True})  # Поле firstName отсутствует
@@ -21,6 +25,8 @@ class TestCreateCourier:
         courier_id = login_response.json().get('id')
         requests.delete(f"{url.DELETE_COURIER}/{courier_id}")
 
+    @allure.story("Ошибка при создании учетной записи курьера без обязательных полей")
+    @allure.title("Создание учетной записи курьера без обязательного поля")
     @pytest.mark.parametrize("data_courier, expected_status_code, expected_response", [
         # Поле login отсутствует
         (data.generate_data_courier(include_first_login=False), 400, {'Недостаточно данных для создания '
@@ -34,6 +40,8 @@ class TestCreateCourier:
 
         assert response.status_code == expected_status_code and response.json().get('message') == expected_response
 
+    @allure.story("Ошибка при повторном создании учетной записи курьера с тем же логином")
+    @allure.title("Попытка создания учетной записи курьера с уже существующим логином")
     def test_create_courier_repeat_login_error(self, create_and_delete_courier):
         data_courier = create_and_delete_courier
 
