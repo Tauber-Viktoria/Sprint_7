41556a231174558a35a30ac82642d712afa7a9c9
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-08-10 23:53:15 +0300

    add class TestLoginCourier: без поля пароль - 504

--- tests/test_courier.py
+++ tests/test_courier.py
@@ -48,3 +48,37 @@ class TestCreateCourier:
         response = requests.post(url.CREATE_COURIER, json=data_courier)
         assert response.status_code == 409 and response.json().get('message') == ('Этот логин уже используется. '
                                                                                   'Попробуйте другой.')
+
+
+class TestLoginCourier:
+    def test_login_courier_successful(self, create_and_delete_courier):
+        data_courier = create_and_delete_courier
+
+        login_response = requests.post(url.LOGIN_COURIER, json=data_courier)
+        courier_id = login_response.json()['id']
+        assert login_response.status_code == 200 and courier_id, "Courier ID not found in login response"
+
+    @pytest.mark.parametrize("remove_field, expected_status_code, expected_message", [
+        # Поле login отсутствует
+        ("login", 400, "Недостаточно данных для входа"),
+        # Поле password отсутствует
+        ("password", 400, "Недостаточно данных для входа")
+    ])
+    def test_login_courier_no_required_field_error_1(self, create_and_delete_courier, remove_field, expected_status_code, expected_message):
+        data_courier = create_and_delete_courier.copy()
+
+        if remove_field in data_courier:
+            data_courier.pop(remove_field)
+
+        login_response = requests.post(url.LOGIN_COURIER, json=data_courier)
+
+        assert login_response.status_code == expected_status_code and login_response.json().get('message') == expected_message
+
+    def test_login_courier_no_required_field_error_2(self, create_and_delete_courier):
+        data_courier = create_and_delete_courier
+
+        data_courier.pop("password")
+
+        login_response = requests.post(url.LOGIN_COURIER, json=data_courier)
+
+        assert login_response.status_code == 400
