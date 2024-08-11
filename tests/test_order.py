35c99d736e96efef98eb442dad931389afc932fc
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-08-11 12:50:41 +0300

    add test_get_order_list

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,3 +1,5 @@
+import random
+
 import allure
 import pytest
 import requests
@@ -5,8 +7,8 @@ import requests
 from helpers import url, data
 
 
-@allure.feature("Создание заказа")
-class TestCreateOrder:
+@allure.feature("Создание заказа и получение списка заказов")
+class TestOrder:
     @pytest.mark.parametrize('color',
                              [data.generate_order_data('BLACK'),
                               data.generate_order_data('GREY'),
@@ -20,3 +22,21 @@ class TestCreateOrder:
         assert (response.status_code == 201
                 and 'track' in response.json()), \
             f"Статус код {response.status_code}, track не найден в ответе"
+
+    @allure.story("Успешное получение списка заказа")
+    @allure.title("Проверка, что API возвращает список заказов. Список не пустой. В списке есть id")
+    def test_get_order_list(self):
+        response = requests.get(url.LIST_ORDERS)
+        response_json = response.json()
+        first_order = response_json['orders'][0]
+        assert (response.status_code == 200
+                and 'orders' in response_json
+                and isinstance(response_json['orders'], list)
+                and len(response_json['orders']) > 0
+                and 'id' in first_order), (
+            f"Ожидался статус код 200 и корректный список заказов с полем 'id', "
+            f"но получили статус код {response.status_code}. "
+            f"В ответе {response_json}"
+        )
+
+
